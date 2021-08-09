28d95220b6de115bc8d093c2b8bcb2d8ca99ec4a
Lobanov88 <lobanov.ilya.1988@gmail.com>
2021-08-09 17:38:34 +0300

    Декоративные элементы главной страницы

--- source/js/menu.js
+++ source/js/menu.js
@@ -6,12 +6,20 @@ burger.addEventListener("click", function() {
   if(navigation.classList.contains("main-nav--close")) {
     navigation.classList.add("main-nav--open");
     navigation.classList.remove("main-nav--close");
+    navigation.classList.remove("main-nav--hidden");
     return;
   }
 
   if(navigation.classList.contains("main-nav--open")) {
-    navigation.classList.add("main-nav--close");
+    navigation.classList.add("main-nav--hidden");
     navigation.classList.remove("main-nav--open");
+    return;
+  }
+
+  if(navigation.classList.contains("main-nav--hidden")) {
+    navigation.classList.add("main-nav--open");
+    navigation.classList.remove("main-nav--hidden");
+    navigation.classList.remove("main-nav--close");
     return;
   }
 
